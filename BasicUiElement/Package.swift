511fa2eb7fab8d0cfa49f766881c9df32081e11e
Baptiste Fortier <bfortier@solem.fr>
2023-10-24 10:23:58 +0200

    WIP

--- BasicUiElement/Package.swift
+++ BasicUiElement/Package.swift
@@ -5,11 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "BasicUiElement",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "BasicUiElement",
-            targets: ["BasicUiElement"]),
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+        name: "BasicUiElement",
+        targets: ["BasicUiElement"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
